e9d5879d0337e6570fd4edc417b19f5ab2ff92de
Nick Colley <nickcolley7@gmail.com>
2025-04-23 22:06:30 +0100

    Add styling for members card

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,12 @@
 const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   mode: 'development',
-  entry: './src/scripts/index.ts',
+  entry: [
+    './src/scripts/index.ts',
+    './src/styles/index.scss'
+  ],
   devtool: 'inline-source-map',
   module: {
     rules: [
@@ -11,10 +15,17 @@ module.exports = {
         use: 'ts-loader',
         exclude: /node_modules/,
       },
+      {
+        test: /\.scss$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      }
     ],
   },
+  plugins: [new MiniCssExtractPlugin({
+    filename: "bundle.css"
+  })],
   resolve: {
-    extensions: ['.ts', '.js'],
+    extensions: ['.ts', '.js', '.scss'],
   },
   devServer: {
     static: './public',
